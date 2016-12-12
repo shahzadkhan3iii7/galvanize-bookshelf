ef8ed43f130e607c6ac9e9aea34be7efc8c732a5
Shahzad Khan <shahzadkhan3iii7@gmail.com>
2016-12-12 15:49:21 -0600

    part 1 linted

--- migrations/20161212144307_books.js
+++ migrations/20161212144307_books.js
@@ -1,26 +1,23 @@
-'use strict';
-exports.up = (knex, Promise) => {
-    return knex.schema.createTable('books', (table) => {
-        table.increments();
-        table.string('title')
-            .notNullable()
-            .defaultTo('');
-        table.string('author')
-            .notNullable()
-            .defaultTo('');
-        table.string('genre')
-            .notNullable()
-            .defaultTo('');
-        table.text('description')
-            .notNullable()
-            .defaultTo('');
-        table.text('cover_url')
-            .notNullable()
-            .defaultTo('');
-        table.timestamps(true, true);
-    })
-};
 
-exports.down = (knex, Promise) => {
-    return knex.schema.dropTable('books');
-};
+
+exports.up = (knex, Promise) => knex.schema.createTable('books', (table) => {
+  table.increments();
+  table.string('title')
+            .notNullable()
+            .defaultTo('');
+  table.string('author')
+            .notNullable()
+            .defaultTo('');
+  table.string('genre')
+            .notNullable()
+            .defaultTo('');
+  table.text('description')
+            .notNullable()
+            .defaultTo('');
+  table.text('cover_url')
+            .notNullable()
+            .defaultTo('');
+  table.timestamps(true, true);
+});
+
+exports.down = (knex, Promise) => knex.schema.dropTable('books');
--- seeds/books.js
+++ seeds/books.js
@@ -1,15 +1,11 @@
-'use strict'
 const data = require('../config/books.js');
-exports.seed = function(knex, Promise) {
+
+exports.seed = function (knex, Promise) {
     // Deletes ALL existing entries
-    return knex('books').del()
-        .then(function() {
-            return Promise.all([
-                // Inserts seed entries
-                knex('books').insert(data.books)
+  return knex('books').del()
+        .then(() => Promise.all([
+            // Inserts seed entries
+          knex('books').insert(data.books),
 
-
-            ]);
-        });
-
+        ]));
 };
