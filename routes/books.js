8f8d12d89a5255d255ea29e2c3789e40b0a89b43
Rally Jinx <rsjinx@gmail.com>
2016-12-13 19:39:19 -0600

    Everything works, tests fail, linted, pushing to the wrong branch

--- routes/books.js
+++ routes/books.js
@@ -1,37 +1,24 @@
-
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const knex = require('../knex');
 
 router.get('/books', (_req, res, next) => {
-
-	knex('books')
-		.orderBy('title')
-		.then((books) => {
-      // res.setHeader('Content-Type', 'application/json');
-      // res.sendStatus(200);
-			res.send(books);
-		})
-		.catch((err) => {
-			next(err);
-		});
+  knex('books')
+    .orderBy('title')
+  	.then((books) => {
+  		res.send(books);
+  	})
+  	.catch((err) => {
+  		next(err);
+  	});
 });
-// const books = require('./routes/books');
 
 router.get('/books/:id', (req, res, next) => {
-  const id = req.params.id;
-    knex('books')
-      .where('id', id)
-      .then((data) => {
-        res.send(data);
-      //   console.log(JSON.stringify(books));
-      // if (!book) {
-      //   return next();
-      // }
-      // res.setHeader('Content-Type', 'application/json');
-      // res.sendStatus(200);
-    //  res.send(book[id]);
+  knex('books')
+    .where('id', req.params.id)
+    .then((book) => {
+      res.send(book[0]);
     })
     .catch((err) => {
       next(err);
@@ -39,14 +26,46 @@ router.get('/books/:id', (req, res, next) => {
 });
 
 router.post('/books', (req, res, next) => {
-
+  knex('books')
+    .insert({ title: req.body.title, author: req.body.author, genre: req.body.genre, description: req.body.description, cover_url: req.body.cover_url }, '*')
+    .then((books) => {
+      res.send(books[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.patch('/books/:id', (req, res, next) => {
-
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+      return knex('books')
+        .update({ title: req.body.title, author: req.body.author, genre: req.body.genre, description: req.body.description, cover_url: req.body.cover_url }, '*')
+        .where('id', req.params.id);
+    })
+    .then((book) => {
+      res.send(book[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.delete('/books/:id', (req, res, next) => {
-
+  knex('books')
+    .del()
+    .where('id', req.params.id)
+    .then(() => {
+      res.send('book deleted');
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
+
 module.exports = router;
